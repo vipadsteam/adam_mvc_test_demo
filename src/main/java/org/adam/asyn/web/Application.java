b3d47f77b3641dc11cf7ae36353452b74e6b5b4d
xiaoruini <xiaoruini@kugou.com>
2019-10-23 11:16:21 +0800

    version gensource test

--- src/main/java/org/adam/asyn/web/Application.java
+++ src/main/java/org/adam/asyn/web/Application.java
@@ -3,6 +3,8 @@
  */
 package org.adam.asyn.web;
 
+import org.adam.gensource.VersionAnnotation;
+import org.adam.version.Version;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,12 +16,15 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 // @ComponentScan(basePackages = { "org.springframework.adam", "org.adam.asyn" })
 @ComponentScan()
+@VersionAnnotation(value="1.1.1", path="D:\\")
 public class Application {
 
 	/**
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.out.println(Version.getVersion());
 		SpringApplication.run(Application.class, args);
 	}
 
